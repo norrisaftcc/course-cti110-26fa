cf0c2a1ecd49dd63d0abab7bed39db1de00ca3d2
teacherbot <norrisa@faytechcc.edu>
2026-09-01 18:39:53 +0000

    p1lab1 finished

--- p1lab1_norris.py
+++ p1lab1_norris.py
@@ -6,3 +6,13 @@
 
 # First, we're going to greet the user
 print("Hello!")
+
+# Next, ask their first name
+#print("What's your name?")
+first_name = input("What's your first name? ")
+
+# do the same thing for last_name
+last_name = input("What's your last name? ")
+
+# Then say back the name
+print("Nice to meet you", first_name, last_name)
