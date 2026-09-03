319805c2e751933c092cd040aa0036c3e3e09494
teacherbot <norrisa@faytechcc.edu>
2026-09-03 18:32:57 +0000

    p1lab finished

--- p1lab2_norris.py
+++ p1lab2_norris.py
@@ -6,14 +6,29 @@
 # Fictional Store -- pick three things
 # product_name, product_count, product_price
 
-# Change these to your own values
-product_name = "apples"   # strings use "quotes" and are made of text
-product_count = 100         # integers are whole numbers, no decimal
-product_price = 1.50       # doubles are decimal numbers
+# Hardcoding sets values directly.
+# product_name = "apples"   # strings use "quotes" and are made of text
+# product_count = 100         # integers are whole numbers, no decimal
+# product_price = 1.50       # floats are decimal numbers
 
+# Instead, we ask the user with input()
+# INPUT
+print("STORE STARTUP")
+print("_" * 10) # ten _ in a row
+product_name = input("Enter product name: ")
+product_count = input("Enter product count: ")
+product_price = input("Enter unit price: ")
+
+# PROCESSING
+product_count = int(product_count)  # convert string to integer: "100" -> 100
+product_price = float(product_price)# convert string to float: "3.25" -> 3.25
+total = product_count * product_price # requires two numbers, returns a third number
+
+# OUTPUT
+print("CUSTOMER INTERFACE")
+print("_" * 10) # ten _ in a row
 print("Welcome to the", product_name, "store")
-print("We have", product_count, product_name, "(s) at $", product_price, "each.")
+# For later -- f string with {variable:.2f} is the magic word to get 2 decimals
+print(f"We have {product_count} {product_name}(s) at ${product_price:.2f} each.")
+print(f"Total is: ${total:.2f}.")
 
-total = product_count * product_price
-
-print("Total is: $",total)
