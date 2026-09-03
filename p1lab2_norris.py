a4a6b3177ff73e3c4454ea0cefe5788f34bc2f65
teacherbot <norrisa@faytechcc.edu>
2026-09-03 17:59:11 +0000

    p1lab2 started

---
+++ p1lab2_norris.py
@@ -0,0 +1,19 @@
+# CTI 110
+# P1LAB2 - Selling Things
+# norrisa
+# 9/3/26
+
+# Fictional Store -- pick three things
+# product_name, product_count, product_price
+
+# Change these to your own values
+product_name = "apples"   # strings use "quotes" and are made of text
+product_count = 100         # integers are whole numbers, no decimal
+product_price = 1.50       # doubles are decimal numbers
+
+print("Welcome to the", product_name, "store")
+print("We have", product_count, product_name, "(s) at $", product_price, "each.")
+
+total = product_count * product_price
+
+print("Total is: $",total)
